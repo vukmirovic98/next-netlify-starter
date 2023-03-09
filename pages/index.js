2e12f722e29e9c01a2cd0ca6e7c3ef501ab96985
Nemanja Vukmirovic <53474043+vukmirovic98@users.noreply.github.com>
2023-03-09 13:52:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
+var axios = require('axios')
 export default function Home() {
   return (
     <div className="container">
